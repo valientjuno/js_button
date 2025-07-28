119fe2ecaee4ead0f484a6983999fee072dd784a
valientjuno <jessedoake91@gmail.com>
2025-07-28 11:42:19 -0600

    count on click js update

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-function addNums(num1, num2) {
-  console.log(num1 + num2);
+// function addNums(num1, num2) {
+//   console.log(num1 + num2);
+// }
+// addNums(0, 1);
+
+let counter = 0;
+let button = document.getElementsByClassName("large red button");
+
+function AddOne(onclick) {
+  counter++;
+  let output = document.getElementById("click");
+
+  console.log("hi");
+
+  console.log(counter);
+  return (output.innerHTML = counter);
 }
-addNums(0, 1);
