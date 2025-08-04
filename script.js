76a8a765eedff86d056825fe907c303136a9a125
valientjuno <jessedoake91@gmail.com>
2025-08-04 12:34:12 -0600

    js update

--- script.js
+++ script.js
@@ -1,17 +1,41 @@
-// function addNums(num1, num2) {
-//   console.log(num1 + num2);
+// // function addNums(num1, num2) {
+// //   console.log(num1 + num2);
+// // }
+// // addNums(0, 1);
+
+// let counter = 0;
+// let button = document.getElementsByClassName("large red button");
+
+// function AddOne(onclick) {
+//   counter++;
+//   let output = document.getElementById("click");
+
+//   console.log("hi");
+
+//   console.log(counter);
+//   return (output.innerHTML = counter);
 // }
-// addNums(0, 1);
+let input = document.getElementById("product");
+let output = document.getElementById("output");
+let productArray = [];
 
-let counter = 0;
-let button = document.getElementsByClassName("large red button");
+function addProduct() {
+  const newProductValue = input.value;
+  if (newProductValue === "") {
+    return;
+  }
+  productArray.push(newProductValue);
+  console.log(productArray);
 
-function AddOne(onclick) {
-  counter++;
-  let output = document.getElementById("click");
+  // Clear previous output to prevent duplicates
+  output.innerHTML = "";
 
-  console.log("hi");
+  // Use forEach to iterate over the entire array and append each item
+  productArray.forEach((product) => {
+    const p = document.createElement("p");
+    p.textContent = product;
+    output.append(p);
+  });
 
-  console.log(counter);
-  return (output.innerHTML = counter);
+  input.value = "";
 }
